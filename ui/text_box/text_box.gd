48aac0b0ead92053d987aadb59f317c22f79b7c8
DevPoodle <richardbonilla112@gmail.com>
2025-05-16 23:51:24 -0700

    Add bottom panel text

---
+++ ui/text_box/text_box.gd
@@ -0,0 +1,22 @@
+extends RichTextLabel
+
+var showing := false
+
+static func is_letter_or_number(c: int) -> bool:
+	var is_letter := (c >= 65 and c <= 90) or (c >= 97 and c <= 122)
+	var is_number := c >= 48 and c <= 57
+	return is_letter or is_number
+
+func show_text() -> void:
+	showing = true
+	visible_characters = 0
+
+func _ready() -> void:
+	show_text()
+
+func _physics_process(_delta: float) -> void:
+	if showing and visible_ratio != 1.0:
+		visible_characters += 1
+		var c := text[visible_characters - 1].to_ascii_buffer()[0]
+		if is_letter_or_number(c):
+			$TypingSound.play()
